2f731b1fec299ef7db6b835fefafb8a09116baf9
MrMakistein <fischli@live.at>
2017-11-01 13:59:13 +0100

    Player Control Thing
    Camera Controller

---
+++ Character Controller/Assets/Scripts/Dash.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour {
+    private float coolDownAmount = 2f;
+    private float coolDownTimer;
+    public float dashSpeed = 10;
+    public float dashDuration = 0.3f;
+    private float dashTimer = 0f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
+
+        if (dashTimer > 0)
+        {
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * dashSpeed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * dashSpeed);
+            dashTimer -= Time.deltaTime;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.LeftShift) && coolDownTimer <= 0 && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+        {
+           
+            dashTimer = dashDuration;
+            coolDownTimer = coolDownAmount;
+        }
+		
+	}
+}
