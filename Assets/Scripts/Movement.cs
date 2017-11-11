68c479b05dc8b17ca544e8ff7df8cdead20625c4
MrMakistein <fischli@live.at>
2017-11-11 21:35:33 +0100

    Shrines

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
     public float mSpeed = 7f;
+    public bool move_block = false;
 
 
 
@@ -14,19 +15,32 @@ public class Movement : MonoBehaviour {
 	
 	void Update () {
 
-        // Moves the player forward if WAS or D is pressed
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        move_block = false;
+
+        GameObject[] shrines = GameObject.FindGameObjectsWithTag("Shrine");
+        foreach (GameObject shrine in shrines)
         {
-            transform.Translate(mSpeed * Vector3.forward * Time.deltaTime);
+            if (shrine.GetComponent<Shrine>().shrine_cooldown_timer > 0)
+            {
+                move_block = true;
+            }
         }
 
-        // Rotates the player into the correct direction
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
-        if (movement != Vector3.zero)
+        if (!move_block)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
-        }
+            // Moves the player forward if WAS or D is pressed
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(mSpeed * Vector3.forward * Time.deltaTime);
+            }
 
+            // Rotates the player into the correct direction
+            Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+            if (movement != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+            }
+        }
         
 
     }
