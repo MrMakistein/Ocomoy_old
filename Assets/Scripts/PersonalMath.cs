24def7ce795e02a6b4eb30837078ba81b2167206
Tyne-PC\Tyne <awd>
2017-11-12 18:59:14 +0100

    Camera && Drag and Drop && Enten

---
+++ Assets/Scripts/PersonalMath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonalMath : MonoBehaviour {
+
+    //These two values are used as there are some gameplay relevant functions that depend on the screen resolution
+    private static float editorScreenMean = (1053 + 459) / 2;
+    private static float playScreenMean;
+
+    // Use this for initialization
+    void Start () {
+        playScreenMean = (Screen.width + Screen.height) / 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static float ScreenSizeCompensation(float toCompensate)
+    {
+        return (toCompensate / editorScreenMean) * playScreenMean;
+    }
+        
+}
---
+++ Assets/Scripts/tornado.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tornado : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public float radius = 19.48f;
+    public float outsideSpeed = 0.7f;
+    public float maxPullInLength = 24.96f;
+    public float power = 1;
+    public float spin = 100;
+
+    Collider[] colliders;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    void Update()
+    {
+        colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider c in colliders)
+        {
+            if (c.GetComponent<Rigidbody>() == null)
+            {
+                continue;
+            }
+            Rigidbody rigidbody = c.GetComponent<Rigidbody>();
+            if (Vector3.Distance(transform.position, c.transform.position) > maxPullInLength)
+            {
+                continue;
+            }
+            Vector3 Force = new Vector3(transform.position.x - c.transform.position.x, rigidbody.velocity.y / 2, transform.position.z - c.transform.position.z) * power;
+            rigidbody.AddForceAtPosition(Force, transform.position);
+            
+            rigidbody.AddForceAtPosition((transform.position - c.transform.position) * power, transform.position);
+            rigidbody.AddForce((transform.position - c.transform.position).normalized * spin);
+        }
+    }
+}
+
+
