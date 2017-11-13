372b320b8f4ca616168c7881518bbb4c9c733822
LeoBsg <25174926+LeoBsg@users.noreply.github.com>
2017-11-13 22:03:15 +0100

    Coding mit Leo am Montag
    Versuch / theorie zu Area Spawn und Winzone determination

---
+++ Assets/Scripts/Areas.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Areas : MonoBehaviour {
+	//private GameObject[] areaInteractives;
+	private List<GameObject> areaInteractives;
+	private static int numberOfAreaObjects = 0; //for unique area id
+	public string name;
+	public bool isWinZone = false;
+
+	// Use this for initialization
+	void Start () {
+		name = "area" + numberOfAreaObjects.ToString; //unique name
+		numberOfAreaObjects++;
+		determineAreaCollectibles ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//save all interactives within the zone into an array
+	private void determineAreaCollectibles() {
+		GameObject[] allInteractives = GameObject.FindGameObjectsWithTag("Interactive");
+		for (int i = 0; i < allInteractives.Length; i++) {
+			if (OnTriggerEnter ((allInteractives [i]).GetComponent<Collider>)) {
+
+			}
+		}
+	}
+		
+	public bool OnTriggerEnter(Collider other){
+		return true;
+	}
+
+	public void setWinZone(){ //for Player spawn and end goal
+		isWinZone = true;
+	}
+
+	//getter for areaCollectibles
+	public GameObject[] getAreaInteractives(){ //TODO oder List returnen?
+		return areaInteractives.ToArray;
+	}
+}
--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -7,15 +7,16 @@ public class SpawnController : MonoBehaviour {
     //PUBLIC CONSTS & VARIABLES
 	public const int numberOfCollectibles = 3; //constant for number of objects to be searched
     public int[] zoneLayerNumbers; //array, numbers of the layers with the zones --> without win zone
+	public GameObject[] collectibles;
+	public GameObject[] areas; //areas in which collectibles spawn (1 per area)
 
     //PRIVATE VARIABLES
     private GameObject[] interactives; //all interactive GameObjects in the scene
 
-
 	// Use this for initialization
 	void Start () {
         SpawnShrines();
-        DetermineCollectibles();
+        //DetermineCollectibles();
     }
 	
 	// Update is called once per frame
@@ -23,26 +24,57 @@ public class SpawnController : MonoBehaviour {
 		
 	}
 
-	void DetermineCollectibles(){
-        interactives = GameObject.FindGameObjectsWithTag("Interactive"); //get all Interactives
-      
+	public void DetermineCollectibles(){
+        //interactives = GameObject.FindGameObjectsWithTag("Interactive"); //get all Interactives
+		List<GameObject> areaObjects = GameObject.FindGameObjectsWithTag("Area");
 
-        ///GET RANDOM GAME OBJECTS FROM THE INTERACTIVES ARRAY    V2
-        var rnd = new System.Random();
+		//choose a random area as the winzone and exclude it from collectible spawn
+		var rnd1 = new System.Random();
+		int winZoneIndex = rnd1.Next (0, (areaObjects.Count) - 1); 
+		GameObject winZone = areaObjects[winZoneIndex];
+		winZone.GetComponent<Areas> ().setWinZone ();
+		areaObjects.Remove (winZone);
+
+		//GameObject[] areas = GameObject.FindGameObjectsWithTag("Area"); //get areas
+		List<GameObject> chosenCollectibles; //List for all collectibles
+		//for (int i = 0; i < interactives.Length; i++) {
+		//foreach(int a in areaObjects) {
+		for (int a = 0; a < areaObjects.Count; a++){
+			
+			//TODO: get interactives in zone array from Zone itself!
+			GameObject zone = areaObjects[a];
+			List<GameObject> interactivesInZone = zone.GetComponent<Areas> ().getAreaInteractives;
+		
+			//choose one of the interactives in the zone randomly
+			var rnd2 = new System.Random ();
+			int n = numberOfCollectibles;
+			for (int i = 0; n > 0; ++i) {
+				int r = rnd2.Next (0, interactivesInZone.Count - i);
+				if (r < n) {
+					chosenCollectibles.Add (interactivesInZone [i]);
+					n--;
+				}
+			} //end set random collectible 
+				
+			//then destroy the zone! (isn't needed anymore)
+			zone.Destroy(); //TODO: check if it works
+		}
+
+
+        ///GET RANDOM GAME OBJECTS FROM THE INTERACTIVES ARRAY
+        /*var rnd2 = new System.Random();
         var chosenCollectibles = new List<GameObject>(); //create empty list for the chosen objects
         int k = numberOfCollectibles;
         for (int i = 0; k > 0; ++i)
         {
-            int r = rnd.Next(0, (interactives.Length)-i);
+            int r = rnd2.Next(0, (interactives.Length)-i);
             if(r < k)
             {
                 chosenCollectibles.Add(interactives[i]);
                 k--;
             }
-        }
-
-
-        GameObject[] collectibles = chosenCollectibles.ToArray(); //to array cuz what r lists
+        }*/
+        collectibles = chosenCollectibles.ToArray(); //to array cuz what r lists
         //print("Number of collectibles: " +collectibles.Length);
         for (int i = 0; i < collectibles.Length; i++)
         {
@@ -57,19 +89,15 @@ public class SpawnController : MonoBehaviour {
         List<int> list = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
         int rand = 0;
         GameObject[] shrines = GameObject.FindGameObjectsWithTag("Shrine");
-        foreach (GameObject shrine in shrines)
-        {
-            rand = UnityEngine.Random.Range(0, list.Count);
-            shrine.gameObject.GetComponent<Shrine>().shrine_id = list[rand];
-            list.RemoveAt(rand);
-        }
-
-
-
-
-
+		foreach (GameObject shrine in shrines) {
+			rand = UnityEngine.Random.Range (0, list.Count);
+			shrine.gameObject.GetComponent<Shrine> ().shrine_id = list [rand];
+			list.RemoveAt (rand);
+		}
     }
-}
+
+
+} //END SpawnController
 
 
 
