408cff659ef6a2153020fbeb8e4fdcbca3588f0b
MrMakistein <fischli@live.at>
2017-11-13 17:08:15 +0100

    Maki Merge

--- Assets/Scripts/dnd.cs
+++ Assets/Scripts/dnd.cs
@@ -44,14 +44,11 @@ public class dnd : MonoBehaviour
     Rigidbody DrObj;
     Vector3 MouseVector;
 
-<<<<<<< HEAD
-=======
     public void ReleaseObject()
     {
         buttonReleased = false;
     }
 
->>>>>>> Maki
     void Start()
     {
         currentCamera = Camera.main;
@@ -85,19 +82,11 @@ public class dnd : MonoBehaviour
 
                 DrObj = draggingObject.GetComponent<Rigidbody>();
 
-<<<<<<< HEAD
-                Debug.Log("Height: " + DrObj.transform.position.y);
-=======
->>>>>>> Maki
                 pickUpScreenPos = currentCamera.WorldToScreenPoint(DrObj.position);
                 MouseVector = PersonalMath.CalculateMouse3DVector(currentCamera, mask, pickUpHeight + heightOffset);
                 //Apply force 
                 if (draggingDrag)
                 {
-<<<<<<< HEAD
-                    Debug.Log("WeightInfluence: " + currentWeightInfluence);
-=======
->>>>>>> Maki
                     DrObj.AddForce((MouseVector - DrObj.transform.position).normalized * forceStrenght, ForceMode.Force);
                     DrObj.drag = (currentWeightInfluence * 1) / Vector3.Distance(DrObj.transform.position, MouseVector);
                 }
@@ -186,10 +175,6 @@ public class dnd : MonoBehaviour
                     heightOffset = HeightOffsetFor4;
                     break;
                 default:
-<<<<<<< HEAD
-                    Debug.LogError("Unknown WeightClass assigned");
-=======
->>>>>>> Maki
                     break;
             }
         }
