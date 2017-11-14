b4298ea8be7221992455c9b22d9ca12ccaaff0da
LeoBsg <25174926+LeoBsg@users.noreply.github.com>
2017-11-14 14:01:58 +0100

    area spawn working :confetti_ball:
    funktionierender area spawn (eins pro area und davon eine Winzone in der nichts gespawnt wird.
    TODO: Winzone von schrein abhängig machen!

--- Assets/Scripts/Areas.cs
+++ Assets/Scripts/Areas.cs
@@ -3,44 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Areas : MonoBehaviour {
-	//private GameObject[] areaInteractives;
-	private List<GameObject> areaInteractives;
+	private List<GameObject> areaInteractives; //all interactives within the area object
 	private static int numberOfAreaObjects = 0; //for unique area id
 	public string name;
 	public bool isWinZone = false;
 
 	// Use this for initialization
 	void Start () {
-		name = "area" + numberOfAreaObjects.ToString; //unique name
-		numberOfAreaObjects++;
-		determineAreaCollectibles ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		//FOR UNIQUE NAME	
+		string id = numberOfAreaObjects.ToString();
+		name = "area" + (numberOfAreaObjects.ToString()); 
+		numberOfAreaObjects++; //count up id
+
+		//INITIALIZE LIST
+		areaInteractives = new List<GameObject>();
 	}
 
-	//save all interactives within the zone into an array
-	private void determineAreaCollectibles() {
-		GameObject[] allInteractives = GameObject.FindGameObjectsWithTag("Interactive");
-		for (int i = 0; i < allInteractives.Length; i++) {
-			if (OnTriggerEnter ((allInteractives [i]).GetComponent<Collider>)) {
-
+	private void OnTriggerEnter(Collider other){ //something entered my area trigger...
+		//FILL ARRAY/LIST FOR INTERACTIVES ON STARTUP
+		if(other.gameObject.CompareTag("Interactive")){ //is it an interactive?
+			if (!this.areaInteractives.Contains(other.gameObject)) { //if not already inside list:
+				this.areaInteractives.Add (other.gameObject); //add!
+				print(name + ": interactive added"); //TESTING
 			}
 		}
-	}
-		
-	public bool OnTriggerEnter(Collider other){
-		return true;
-	}
+
+		print (name +": TriggerEnter Executed!"); //TESTING
+	} //end onTriggerEnter
 
 	public void setWinZone(){ //for Player spawn and end goal
 		isWinZone = true;
+		this.GetComponent<Renderer> ().material.color = Color.green;
+		float opacity = 0.5f;
+		this.GetComponent<Renderer> ().material.color = new Color(0, 1, 0, 0.5f);
 	}
 
-	//getter for areaCollectibles
-	public GameObject[] getAreaInteractives(){ //TODO oder List returnen?
-		return areaInteractives.ToArray;
+	//getter for areaCollectibles (LIST)
+	public List<GameObject> getAreaInteractives(){ 
+		return areaInteractives;
+	}
+
+	//getter for areaCollectables (ARRAY)
+	public GameObject[] getAreaInteractivesArray(){
+		return areaInteractives.ToArray ();
 	}
 }
